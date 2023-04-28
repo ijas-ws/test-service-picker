87a5546260f71f5f85eccde34fb908311ca2724d
Ijas Mohamad <ijas@wednesday.is>
2023-04-28 20:38:17 +0530

    feat(test): First commit

---
+++ parseSstOutputs.js
@@ -0,0 +1,19 @@
+import * as fs from "fs";
+
+function parseOutputs() {
+  const outputFile = "./.sst/outputs.json";
+  let fileContent = JSON.parse(fs.readFileSync(outputFile, "utf-8"));
+
+  Object.keys(fileContent).some((k) => {
+    if (k.endsWith("Pg") || k.endsWith("Mysql")) {
+      if (fileContent[k]?.logdriveroptions) {
+        fileContent[k].logdriveroptions = JSON.parse(
+          fileContent[k].logdriveroptions
+        );
+      }
+    }
+  });
+  fileContent = JSON.stringify(fileContent, null, 2);
+  fs.writeFileSync(outputFile, fileContent);
+}
+parseOutputs();
